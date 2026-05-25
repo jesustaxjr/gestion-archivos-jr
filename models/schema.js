f904a596594c8d8efc0857416b32140a28a1cdeb
jtaxs <jtaxs@miumg.edu.gt>
2026-05-24 19:13:13 -0600

    chore: actualizar credenciales de usuarios de prueba

--- models/schema.js
+++ models/schema.js
@@ -3,14 +3,13 @@ const bcrypt = require('bcryptjs');
 
 function initDatabase() {
     db.serialize(() => {
-        // 1. Crear Tabla de Usuarios
+        // 1.Tabla usuario y archivos
         db.run(`CREATE TABLE IF NOT EXISTS usuarios (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT UNIQUE,
             password TEXT
         )`);
 
-        // 2. Crear Tabla de Archivos
         db.run(`CREATE TABLE IF NOT EXISTS archivos (
             id TEXT PRIMARY KEY,
             usuario_id INTEGER,
@@ -19,13 +18,13 @@ function initDatabase() {
             FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
         )`);
 
-        // 3. Insertar los dos usuarios de prueba requeridos
+        // se ingresan los 2 usuarios requeridos.
         const salt = bcrypt.genSaltSync(10);
         // Contraseñas encriptadas por seguridad
         const passHash1 = bcrypt.hashSync('user123', salt);
         const passHash2 = bcrypt.hashSync('user456', salt);
 
-        // Usamos INSERT OR IGNORE para que no duplique los usuarios si reinicias el servidor
+        // Usamos INSERT OR IGNORE para que duplicaciones si reinicia el servidor
         db.run(`INSERT OR IGNORE INTO usuarios (username, password) VALUES ('usuario1', '${passHash1}')`);
         db.run(`INSERT OR IGNORE INTO usuarios (username, password) VALUES ('usuario2', '${passHash2}')`);
     });
