a774c6d0195e0f2985a5c066c1145f7a3a606fbb
jesustaxjr <jesustaxsapon@gmail.com>
2026-05-24 19:20:57 -0600

    Update authentication middleware comments for clarity

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     if (req.session && req.session.userId) {
-        return next(); // Usuario autenticado, continúa
+        return next(); // Si el cliente se autentica de manera correcta continua.
     }
-    return res.redirect('/login'); // No autenticado, lo enviamos al login
+    return res.redirect('/login'); // No autenticado se le envia al login.
 };
